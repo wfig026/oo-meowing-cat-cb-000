e8791c60b861a1e160770a48dc7bb8abc91c0af3
Will Figueiredo <will@wfigueiredo.com>
2018-10-18 00:41:03 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,14 +1,14 @@
 ## code your solution here.
 class Cat
   attr_accessor :name
-
-  def meow=(meow)
-    @meow = meow
-  end
-
-  def meow
-    @meow
-  end
+  attr_accessor :meow
+  # def meow=(meow)
+  #   @meow = meow
+  # end
+  #
+  # def meow
+  #   @meow
+  # end
 end
 
 maru = Cat.new
